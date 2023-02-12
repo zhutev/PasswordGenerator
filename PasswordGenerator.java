3e25f45e82ad803d4a28d3e2b0e9243e39fe6229
zhutev <vladimir.zhutev@gmail.com>
2023-02-12 17:58:15 +0200

    Code update
    Have added the code of the program

---
+++ PasswordGenerator.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+        int length = 10; // length of the password
+        System.out.println(generatePassword(length));
+    }
+
+    static char[] generatePassword(int length) {
+        System.out.println("Generating password using random() : ");
+        System.out.print("Your new password is : ");
+
+        // A strong password has Cap_chars, Lower_chars,
+        // numeric value and symbols. So we are using all of
+        // them to generate our password
+        String capitalChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String smallChars = "abcdefghijklmnopqrstuvwxyz";
+        String numbers = "0123456789";
+        String symbols = "!@#$%^&*_=+-/.?<>)";
+
+        String values = capitalChars + smallChars +
+                numbers + symbols;
+
+        // Using random method
+        Random randomMethod = new Random();
+
+        char[] password = new char[length];
+
+        for (int i = 0; i < length; i++) {
+            // Use of charAt() method : to get character value
+            // Use of nextInt() as it is scanning the value as int
+            password[i] =
+                    values.charAt(randomMethod.nextInt(values.length()));
+        }
+        return password;
+    }
+}
+
---
+++ out/production/PasswordGenerator/README.md
@@ -0,0 +1,6 @@
+# PasswordGenerator
+Basic Java password generator
+
+This program generates a random password of the specified length (10 characters in this case). 
+The password consists of a combination of uppercase letters, lowercase letters, numbers, and symbols. 
+You can adjust the password length and the set of characters used to generate the password by modifying the corresponding variables in the code.
